6743e11833c30559c852026d1edc6c4a2a9feecc
Frederico Brigido <fredericobrigido@gmail.com>
2026-08-15 02:16:14 -0300

    adicionando exercicio 1 de POO

---
+++ src/main/java/exercicio1_POO/Program.java
@@ -0,0 +1,31 @@
+package exercicio1_POO;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        Triangulo y= new Triangulo();
+        Triangulo x= new Triangulo();
+
+        System.out.println("Insira os valores do Triangulo X");
+        x.lado1 = sc.nextDouble();
+        x.lado2 =sc.nextDouble();
+        x.lado3 = sc.nextDouble();
+
+        System.out.println("Insira os valores do Triangulo y");
+        y.lado1 = sc.nextDouble();
+        y.lado2 =sc.nextDouble();
+        y.lado3 = sc.nextDouble();
+
+        //chamando metodo para calcular a area dos triangulos
+        double areaX = x.area();
+
+        double areaY = y.area();
+
+        System.out.println("Area de X: " + areaX);
+        System.out.println("Area de Y: " + areaY);
+    }
+}
---
+++ src/main/java/exercicio1_POO/Triangulo.java
@@ -0,0 +1,15 @@
+package exercicio1_POO;
+
+public class Triangulo {
+
+    //adicionando atributos publicos para ser acessado ao Program
+    public double lado1, lado2, lado3;
+
+    //criando metodo que para calcular a area do triangulo
+    public double area(){
+        double p = (lado1 + lado2 + lado3) / 2.0;
+        return Math.sqrt(p * (p - lado1) * (p - lado2) * (p - lado3));
+    }
+
+
+}
